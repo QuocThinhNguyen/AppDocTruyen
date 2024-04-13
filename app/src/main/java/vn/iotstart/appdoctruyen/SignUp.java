4930f17469de06aa162020d8da5b4a556024845f
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-04-13 22:24:43 +0700

    Thien Tien

---
+++ app/src/main/java/vn/iotstart/appdoctruyen/SignUp.java
@@ -0,0 +1,14 @@
+package vn.iotstart.appdoctruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
